a5f023b7b6d9ce3cf39adeb4d36140828520bd3a
Alex <alexplaza0495@gmail.com>
2023-07-14 00:13:37 -0700

    add files for heroku

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BlogPost from './components/BlogPost';
 
+const DummyComponent: React.FC = () => {
+    return <p>hey</p>;
+}
 const App: React.FC = () => {
     return (
         <Router>
             <Route exact path="/post/:title" component={BlogPost} />
+            <Route exact path="/" component={DummyComponent} />
         </Router>
     );
 };
