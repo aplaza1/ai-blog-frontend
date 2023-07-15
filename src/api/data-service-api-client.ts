6332403b2b014a02e75d5976af2836fc8e083845
Alex <alexplaza0495@gmail.com>
2023-07-14 20:03:20 -0700

    update api

--- src/api/data-service-api-client.ts
+++ src/api/data-service-api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BlogPost } from '../types/BlogPost';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://ai-blog-data-service-2902779ebc41.herokuapp.com';
 
 export const getBlogPost = async (title: string): Promise<BlogPost> => {
     const { data } = await axios.get(`${BASE_URL}/blog-posts`, {
