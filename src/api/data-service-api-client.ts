5d2aba831cb64f5b31bdb5477a7eed235d49cc14
Alex <alexplaza0495@gmail.com>
2023-07-18 00:34:32 -0700

    change to path param

--- src/api/data-service-api-client.ts
+++ src/api/data-service-api-client.ts
@@ -4,11 +4,6 @@ import { BlogPost } from '../types/BlogPost';
 const BASE_URL = 'https://ai-blog-data-service-2902779ebc41.herokuapp.com';
 
 export const getBlogPost = async (title: string): Promise<BlogPost> => {
-    const { data } = await axios.get(`${BASE_URL}/blog-posts`, {
-        params: {
-            title
-        }
-    });
-
+    const { data } = await axios.get(`${BASE_URL}/blog-posts/${title}`);
     return data;
 };
