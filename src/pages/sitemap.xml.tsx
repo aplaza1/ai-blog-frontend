d97a5172dd0aaab6e5457d31065823784932a061
Alex <alexplaza0495@gmail.com>
2023-07-19 23:16:37 -0700

    add site map

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,32 @@
+import {GetServerSideProps} from "next";
+import {getBlogPosts} from "../api/data-service-api-client";
+
+export default function Sitemap() {
+    return null
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const xml = generateSiteMap();
+    ctx.res.setHeader('Content-Type', 'text/xml');
+    ctx.res.write(xml);
+    ctx.res.end();
+    return {
+        props: {},
+    }
+}
+
+async function generateSiteMap(): Promise<string> {
+    const pages = await getBlogPosts();
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages.map(page => {
+            return `
+              <url>
+                <loc>https://www.postscribbled.com/posts/${page.title}</loc>
+              </url>
+            `;
+        })
+        .join('')}
+    </urlset>`
+      ;
+}
